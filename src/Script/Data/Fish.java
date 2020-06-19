4b6dd8122b54781d9fb2640dd85dfdf9271f4ae0
Joshua Farin <farin.k.joshua@gmail.com>
2020-06-18 21:54:21 -1000

    Update data

--- src/Script/Data/Fish.java
+++ src/Script/Data/Fish.java
@@ -1,4 +1,38 @@
 package Script.Data;
 
-public class Fish {
+public enum Fish {
+    SHRIMPS(1, "Raw shrimps", "Fishing spot"),
+    SARDINES(5, "Raw sardines", "Fishing spot"),
+    HERRING(10, "Raw herring", "Fishing spot"),
+    ANCHOVIES(15,),
+    TROUT(20,),
+    PIKE(25,),
+    SALMON(30,),
+    TUNA(),
+    LOBSTER(),
+    SWORDFISH()
+    ;
+
+
+    private int Level;
+    private String fishName;
+    private String spotName;
+
+    Fish(int level, String fishName, String spotName) {
+        Level = level;
+        this.fishName = fishName;
+        this.spotName = spotName;
+    }
+
+    public int getLevel() {
+        return Level;
+    }
+
+    public String getFishName() {
+        return fishName;
+    }
+
+    public String getSpotName() {
+        return spotName;
+    }
 }
